7ef6445928b4a6a949029330bc483fc51185d13a
Louis Kold <louiskolding@live.dk>
2024-10-12 14:43:26 +0200

    + forgot this

--- src/Player.cpp
+++ src/Player.cpp
@@ -32,27 +32,49 @@ sf::Vector2f Player::getPosition() {
 }
 
 void Player::selectUnit(std::shared_ptr<Entity> &entity) {
-    this->selectedEntities[entity->getComponent<UUIDComponent>()->ID] = std::weak_ptr<Entity>(entity);
+    if (entity->hasComponent<SelectableComponent>())
+        this->selectedEntities[entity->getComponent<UUIDComponent>()->ID] = std::weak_ptr<Entity>(entity);
 }
 
 void Player::deselectUnit(std::shared_ptr<Entity> &entity) {
     this->selectedEntities.erase(entity->getComponent<UUIDComponent>()->ID);
 }
 
-void Player::update(float deltaTime) {
-    //  Follow entity
+void Player::update(float deltaTime, std::shared_ptr<InputManager> inputManager) {
+    //  Follow entity if pointer is valid
     if (auto ent_p = this->entityFollow.lock()) {
         auto ent_pos = sf::Vector2f(ent_p->getComponent<PositionComponent>()->x, ent_p->getComponent<PositionComponent>()->y);
         auto pla_pos = this->getPosition();
 
         if (ent_p->getComponent<PositionComponent>()->x != pla_pos.x) {
-            this->setPosition(ent_pos.x);
+            this->setPosition(ent_pos.x);//  <- set view directly to entity coords
         } 
 
         if (ent_p->getComponent<PositionComponent>()->y != pla_pos.y) {
-            this->setPosition(0, ent_pos.y);
+            this->setPosition(0, ent_pos.y);//  <- and here
         }
     }
+    // else check for input
+    float moveX = 0;
+    float moveY = 0;
+    if (inputManager->keyState[sf::Keyboard::W]) { moveY -= 1.0f; } // up
+    if (inputManager->keyState[sf::Keyboard::A]) { moveX -= 1.0f; } // left
+    if (inputManager->keyState[sf::Keyboard::S]) { moveY += 1.0f; } // down
+    if (inputManager->keyState[sf::Keyboard::D]) { moveX += 1.0f; } // right
+
+    if (inputManager->mouseWheelScroll > 0.0f && this->playerView.getSize().x > 500) {
+        this->playerView.zoom(0.9f);  // Zoom in
+    } 
+    if (inputManager->mouseWheelScroll < 0.0f && this->playerView.getSize().x < 900) {
+        this->playerView.zoom(1.1f);  // Zoom out
+    }
+    // reset mWheelScrollDelta
+    inputManager->mouseWheelScroll = 0.0f;
+    
+    // update player
+    if (moveX != 0.0f || moveY != 0.0f )
+        this->move(moveX, moveY, deltaTime);
+
 }
 
 void Player::followUnit(std::shared_ptr<Entity>& entity) {
